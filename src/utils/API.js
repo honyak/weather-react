c006d9ab3769e07468818bb615352b2ec92123f4
Nick Honyak <nicholas@honyak.com>
2021-07-07 21:39:58 -0600

    fixed https requst

--- src/utils/API.js
+++ src/utils/API.js
@@ -5,20 +5,23 @@ const google = {
 }
 
 const openweather = {
-    BASEURL: 'http://api.openweathermap.org/data/2.5/forecast?',
+    BASEURL: 'https://api.openweathermap.org/data/2.5/forecast?',
     APIKEY: 'cnt=10&units=imperial&appid=9d4cd5d4d8f7ff93e056ff8b08335fff'
 }
-export default {
+const API = {
 
     getGeocode: function (zip) {
         return axios.get(google.BASEURL + zip + google.APIKEY);
     },
 
     oneCall: function(long, lat) {
-        
+
         let latitude = `lat=${lat}&`
         let longitude = `lon=${long}&`;
         // console.log(openweather.BASEURL + latitude + longitude + openweather.APIKEY);
         return axios.get(openweather.BASEURL + latitude + longitude + openweather.APIKEY);
     }
+
 }
+
+export default API;
